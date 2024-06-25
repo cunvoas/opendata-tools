6004196856cdac6fa717fee4a666d6bbd9c0c371
cunvoas <christophe.unvoas@gmail.com>
2024-06-25 16:55:28 +0200

    feat: mass import carre 200m

--- geoservice-isochrone/src/main/java/com/github/cunvoas/geoserviceisochrone/extern/geojson/MassGeoJsonIntegratorParser.java
+++ geoservice-isochrone/src/main/java/com/github/cunvoas/geoserviceisochrone/extern/geojson/MassGeoJsonIntegratorParser.java
@@ -20,102 +20,104 @@ import com.github.cunvoas.geoserviceisochrone.repo.reference.InseeCarre200mOnlyS
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ *  Import très lent: 17000 lignes/h, soit 33j d'import.
+ *  
+ *  Optimisation > direct en base
+ *  @see massImportCarreau200mSeuls.sql
+ */
 @Component
 @Slf4j
+@Deprecated
 public class MassGeoJsonIntegratorParser {
-	
+
 	@Autowired
 	private GeoJson2GeometryHelper geoParser;
 
 	@Autowired
 	private Filosofil200mRepository filosofil200mRepository;
-	
+
 	@Autowired
 	private InseeCarre200mOnlyShapeRepository inseeCarre200mOnlyShapeRepository;
-	
+
 	/**
 	 * Parse INSPIRE geojson file.
+	 * 
 	 * @param fGeojson
 	 */
 	public void parseAndSave(String fGeojson) {
-		
 
-//        
-        String theLine=null;
-        BufferedReader br =null;
-        Reader rGeojson =null;
-        List<InseeCarre200mOnlyShape> shapes = new ArrayList<>(100);
-        try {
-           rGeojson = new FileReader(fGeojson);
-           // open input stream test.txt for reading purpose.
-           br = new BufferedReader(rGeojson);
-           
-           int i=0;
-           while ((theLine = br.readLine()) != null) {
-              
-              InseeCarre200mOnlyShape shape = this.processLine(theLine);
-              if (shape!=null) {
-            	  shapes.add(shape);
-            	  i++;
-              }
-             
-              
-              if (i==100) {
-            	  // batch update
-        		  inseeCarre200mOnlyShapeRepository.saveAll(shapes);
-            	  shapes.clear();
-            	  i=0;
-              }
-           }       
-           
-           // ens the last uncomplete block
-           if (!shapes.isEmpty()) {
-        	   inseeCarre200mOnlyShapeRepository.saveAll(shapes);
-           }
-           
-        } catch(IOException e) {
-        	log.error("err:{}", theLine);
-        } 
-     
-	
+		String theLine = null;
+		BufferedReader br = null;
+		Reader rGeojson = null;
+		List<InseeCarre200mOnlyShape> shapes = new ArrayList<>(100);
+		try {
+			rGeojson = new FileReader(fGeojson);
+			// open input stream test.txt for reading purpose.
+			br = new BufferedReader(rGeojson);
 
-//		  Path filePath = Paths.get(fGeojson);
-//		  Charset charset = StandardCharsets.UTF_8;
-//        
-//        try (Stream<String> linesStream = Files.lines(filePath, charset)) {
-//            linesStream.forEach(theLine -> {
-//            	InseeCarre200mOnlyShape shape = this.processLine(theLine);
-//
-//            	
-//            });
-//        }
+			long start = System.currentTimeMillis();
+			System.out.println();
+			int i = 0;
+			while ((theLine = br.readLine()) != null) {
+
+				InseeCarre200mOnlyShape shape = this.processLine(theLine);
+				if (shape != null) {
+					shapes.add(shape);
+					i++;
+				}
+
+				if (i == 100) {
+					// batch update
+					inseeCarre200mOnlyShapeRepository.saveAll(shapes);
+					shapes.clear();
+					i = 0;
+				}
+			}
+
+			// ens the last uncomplete block
+			if (!shapes.isEmpty()) {
+				inseeCarre200mOnlyShapeRepository.saveAll(shapes);
+			}
+
+			System.out.println(System.currentTimeMillis() - start);
+
+		} catch (IOException e) {
+			log.error("err:{}", theLine);
+		}
+
 	}
-	
+
+	/**
+	 * @param theLine
+	 * @return
+	 */
 	public InseeCarre200mOnlyShape processLine(String theLine) {
-		InseeCarre200mOnlyShape shape =null;
-    	try {
-    		GeoJsonTmp idInspire = geoParser.parseInspire(theLine);
-    		
-    		shape = new InseeCarre200mOnlyShape();
-    		shape.setIdInspire(idInspire.idInspire);
-    		shape.setIdCarre1km(idInspire.id1km);
-    		if (idInspire.geometry!=null) {
-        		shape.setGeoShape(idInspire.geometry);
+		InseeCarre200mOnlyShape shape = null;
+		try {
+			GeoJsonTmp idInspire = geoParser.parseInspire(theLine);
+
+			shape = new InseeCarre200mOnlyShape();
+			shape.setIdInspire(idInspire.idInspire);
+			shape.setIdCarre1km(idInspire.id1km);
+			if (idInspire.geometry != null) {
+				shape.setGeoShape(idInspire.geometry);
+				// gain de 2% en retirant ce bloc
         		shape.setGeoPoint2d(idInspire.geometry.getCentroid());
-    		}
-    		
+			}
+
+			// gain max 15% en retirant ce bloc
     		List<Filosofil200m> filo = filosofil200mRepository.findByIdInspire(idInspire.idInspire);
     		if (filo!=null && !filo.isEmpty()) {
     			shape.setWithPop(Boolean.TRUE);
     		} else {
     			shape.setWithPop(Boolean.FALSE);
     		}
-			
+
 		} catch (JsonProcessingException e) {
 			log.info("err:{}", theLine);
 		}
-    	return shape;
+		return shape;
 	}
-	
 
 }
--- geoservice-isochrone/src/main/java/com/github/cunvoas/geoserviceisochrone/repo/reference/InseeCarre200mOnlyShapeRepository.java
+++ geoservice-isochrone/src/main/java/com/github/cunvoas/geoserviceisochrone/repo/reference/InseeCarre200mOnlyShapeRepository.java
@@ -14,16 +14,16 @@ import com.github.cunvoas.geoserviceisochrone.model.opendata.InseeCarre200mShape
 public interface InseeCarre200mOnlyShapeRepository extends JpaRepository<InseeCarre200mOnlyShape, String>{
 	
 	
-	@Query(nativeQuery = true, value =  "SELECT * FROM carre200onlyshape WHERE ST_Intersects(geo_shape, ?1)")
-	List<InseeCarre200mOnlyShape> findCarreInMapArea(String polygon);
-
-	
-	public static final String FIND_PARK_AREAS = 
-			"SELECT s.id_inspire, s.id_carre_1km, c.geo_point_2d, s.geo_shape, p.code_insee, p.name "+
-			"FROM park_area p, carre200onlyshape s INNER JOIN carre200 c ON s.id_inspire=c.id_inspire " +
-			"WHERE ST_Intersects(s.geo_shape, :mapArea) AND ST_intersects(p.polygon, s.geo_shape) " +
-			"ORDER BY s.id_carre_hab";
-	@Query(value = FIND_PARK_AREAS, nativeQuery = true)
-	public List<Object[]> findAreasInMapArea(@Param("annee") Integer annee, @Param("mapArea") String mapArea);
+//	@Query(nativeQuery = true, value =  "SELECT * FROM carre200onlyshape WHERE ST_Intersects(geo_shape, ?1)")
+//	List<InseeCarre200mOnlyShape> findCarreInMapArea(String polygon);
+//
+//	
+//	public static final String FIND_PARK_AREAS = 
+//			"SELECT s.id_inspire, s.id_carre_1km, c.geo_point_2d, s.geo_shape, p.code_insee, p.name "+
+//			"FROM park_area p, carre200onlyshape s INNER JOIN carre200 c ON s.id_inspire=c.id_inspire " +
+//			"WHERE ST_Intersects(s.geo_shape, :mapArea) AND ST_intersects(p.polygon, s.geo_shape) " +
+//			"ORDER BY s.id_carre_hab";
+//	@Query(value = FIND_PARK_AREAS, nativeQuery = true)
+//	public List<Object[]> findAreasInMapArea(@Param("annee") Integer annee, @Param("mapArea") String mapArea);
 
 }
