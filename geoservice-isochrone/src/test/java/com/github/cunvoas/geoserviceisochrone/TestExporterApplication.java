cb1db9a9d4f53b370f2a1f93b4bab3c7c2cb4a29
cunvoas <christophe.unvoas@gmail.com>
2026-01-10 18:29:16 +0100

    feat: add toggle feature for mobile apis

--- geoservice-isochrone/src/test/java/com/github/cunvoas/geoserviceisochrone/TestExporterApplication.java
+++ geoservice-isochrone/src/test/java/com/github/cunvoas/geoserviceisochrone/TestExporterApplication.java
@@ -30,6 +30,7 @@ import com.github.cunvoas.geoserviceisochrone.service.opendata.ServiceIris;
 cp -Rf /var/isochrone/data/* /work/PERSO/github/gh_pages/geoservice-data
 cd /work/PERSO/github/gh_pages/geoservice-data
 git status
+
  */
 class TestExporterApplication {
 	
@@ -47,6 +48,7 @@ class TestExporterApplication {
 	private ServiceIris serviceIris;
 
 	@Test
+	@Disabled
 	@Order(1)
 	void hostname() {
 		try {
@@ -114,19 +116,18 @@ class TestExporterApplication {
 	void writeLiveDataByCom2coAndYear() {
 		
 		Integer[] annees= {2015,2017,2019};
-//		Integer annee=2019;
 		Long com2coId=1l;
 		
 		CommunauteCommune com2co=serviceReadReferences.getCommunauteCommuneById(com2coId);
+
+//		Integer annee=2019;
+//		export( com2co, annee);
+		
 		try {
 
-//			export( com2co, annee);
-			
 			for (Integer anneeIter : annees) {
 				export( com2co, anneeIter);
-				
 			}
-			
 			
 
 		} catch (IOException e) {
