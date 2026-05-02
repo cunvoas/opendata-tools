e0bb3791f90ac5659146f90395a68056e19298f2
cunvoas <christophe.unvoas@gmail.com>
2026-05-02 08:01:18 +0200

    test(various): add for memory

--- geoservice-isochrone-v2/src/test/java/com/github/cunvoas/geoserviceisochrone/OverpassIntegration.java
+++ geoservice-isochrone-v2/src/test/java/com/github/cunvoas/geoserviceisochrone/OverpassIntegration.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 import java.io.IOException;
 import java.nio.file.Path;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,13 +20,26 @@ class OverpassIntegration {
     
     @Autowired
     private OverpassParser parser;
-
-
-
+    
     @Test
     void testParseElements_Way_withDemoGeoJson() throws Exception {
         try {
-            Path p = Path.of("/work/PERSO/ASSO/overpass-turbo/HdF.geo.json");
+            Path p = Path.of("/work/PERSO/ASSO/overpass-turbo/mel.geo.json");
+            parser.parseEntityFromPath(p);
+            
+            p = Path.of("/work/PERSO/ASSO/overpass-turbo/HdF2.geo.json");
+            parser.parseEntityFromPath(p);
+            
+            p = Path.of("/work/PERSO/ASSO/overpass-turbo/lyon.geo.json");
+            parser.parseEntityFromPath(p);
+            
+            p = Path.of("/work/PERSO/ASSO/overpass-turbo/nantes.geo.json");
+            parser.parseEntityFromPath(p);
+            
+            p = Path.of("/work/PERSO/ASSO/overpass-turbo/toulouse.geo.json");
+            parser.parseEntityFromPath(p);
+            
+            p = Path.of("/work/PERSO/ASSO/overpass-turbo/paris_grand.geo.json");
             parser.parseEntityFromPath(p);
         } catch (IOException e) {
             fail(e);
