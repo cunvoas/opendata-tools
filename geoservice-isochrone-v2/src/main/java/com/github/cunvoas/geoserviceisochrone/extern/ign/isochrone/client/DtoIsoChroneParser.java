1e59ddb854fec698c09e29650d53d0c374271960
cunvoas <christophe.unvoas@gmail.com>
2026-07-14 22:47:55 +0200

    feat(proposal): add results in squares

--- geoservice-isochrone-v2/src/main/java/com/github/cunvoas/geoserviceisochrone/extern/ign/isochrone/client/DtoIsoChroneParser.java
+++ geoservice-isochrone-v2/src/main/java/com/github/cunvoas/geoserviceisochrone/extern/ign/isochrone/client/DtoIsoChroneParser.java
@@ -6,8 +6,6 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.github.cunvoas.geoserviceisochrone.extern.ign.isochrone.client.dto.DtoConstraint;
 import com.github.cunvoas.geoserviceisochrone.extern.ign.isochrone.client.dto.DtoCoordinate;
 import com.github.cunvoas.geoserviceisochrone.extern.ign.isochrone.client.dto.DtoIsoChrone;
